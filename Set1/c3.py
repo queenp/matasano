d5cef1da9126b23aeb289dff5c1ec9710c810f93
Phoebe Queen <foibey@gmail.com>
2015-09-22 22:40:50 +0100

    challenge 3 solved

--- Set1/c3.py
+++ Set1/c3.py
@@ -1,14 +1,43 @@
 #!/usr/bin/python
 import string
 import sys
-from helpers import lxor_buf
+from collections import Counter
+from helpers import *
+
+# A fast/lazy/stupid way to turn up some good candidates by eyeball.
+# No good for searching through bulk for xored strings amongst binary chaff
+# OUTPUT:
+# $ ./c3.py 1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+# 47 : \pptvqx?R\8l?svtz?~?opjq{?py?}~|pq
+# 4a : Q}}y{|u2_Q5a2~{yw2s2b}g|v2}t2psq}|
+# 4d : Vzz~|{r5XV2f5y|~p5t5ez`{q5zs5wtvz{
+# 4f : Txx|~yp7ZT0d7{~|r7v7gxbys7xq7uvtxy
+# 50 : Kggcafo(EK/{(dacm(i(xg}fl(gn(jikgf
+# 51 : Jffb`gn)DJ.z)e`bl)h)yf|gm)fo)khjfg
+# 53 : Hdd`bel+FH,x+gb`n+j+{d~eo+dm+ijhde
+# 55 : Nbbfdcj-@N*~-adfh-l-}bxci-bk-olnbc
+# 56 : Maaeg`i.CM)}.bgek.o.~a{`j.ah.loma`
+# 58 : Cooking MC's like a pound of bacon
+# 59 : Bnnjhof!LB&r!mhjd!`!qntoe!ng!c`bno
+# 5a : Ammikle"OA%q"nkig"c"rmwlf"md"`caml
+# ....
+def brute_xor(buff):
+    for x in range(0,256):
+        candidate = lxor_buf(buff,chr(x))
+        if not len([c for c in candidate if c not in string.printable]) > 0:
+            print chr(x).encode('hex') + " : " + candidate
+
 
 def main():
     ciphertext = sys.argv[1].decode('hex') # straight to binary buffer
+    best_cand = ""
+    best_xor = ""
     for x in range(0,256):
         candidate = lxor_buf(ciphertext,chr(x))
-        if not len([c for c in candidate if c not in string.printable]) > 0:
-            print chr(x).encode('hex') + " : " + candidate
+        if best_cand == "" or score_freq(candidate) < score_freq(best_cand):
+            best_cand = candidate
+            best_xor = hex(x)
+    print best_xor + " : " + best_cand
 
 if __name__ == "__main__":
     main()
--- Set1/helpers.py
+++ Set1/helpers.py
@@ -1,3 +1,11 @@
+from collections import Counter, defaultdict
+
+# list of characters in text ordered by frequency from most common to least
+expected_order = ' etaoinsrhldcumfgpyw\r\nb,.vk-"_\'x)(;0j1q=2:z/*!?$35>{}49[]867\\+|&<%@#^`~'
+
+def allowed(buff, allowable = expected_order):
+    return (len([c for c in buff.lower() if c not in allowable]) == 0)
+
 # XOR 2 equal length buffers
 def xor_buf(buf_a, buf_b):
     buf_ret = [chr(ord(a)^ord(b)) for a,b in zip(buf_a, buf_b)]
@@ -7,3 +15,20 @@ def xor_buf(buf_a, buf_b):
 def lxor_buf(buf_a, buf_l):
     loop_buf = buf_l * (len(buf_a)/len(buf_l)) + buf_l[:(len(buf_a) % len(buf_l))]
     return "".join(xor_buf(buf_a,loop_buf))
+
+# digest a candidate to dict of chars and frequencies
+def char_freq_count(cand):
+    counts = Counter(cand)
+    return counts
+
+# Score based on ordering of common letters. Lower is more "normative" English.
+# This scoring mechanism *seriously* exaggerates unusual chars.
+# Which is good because failed decryption probably has glaring incorrectnesses
+def score_freq_order(cand):
+    points = defaultdict(lambda: 100)
+    for i in range(len(expected_order)):
+        points[expected_order[i]] = i
+    score = 0
+    for c in cand.lower():
+        score += points[c]
+    return score
